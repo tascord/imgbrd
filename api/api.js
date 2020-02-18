2c02b51004add80024cdb05412064f3ddaf435b0
Oscar Hill <theactivesquare@gmail.com>
2020-02-19 09:08:41 +1100

    funi buni
    now works liberals

--- api/api.js
+++ api/api.js
@@ -1,5 +1,23 @@
 const fs = require('fs');
-const posts = require('posts');
+var posts = JSON.parse(fs.readFileSync('./api/posts.json'));
+
+// ----------------------------------------------------------------------
+
+class Post {
+
+    localPath = '';
+    captionText = '';
+    authorName = '';
+    id = '';
+
+    constructor(imageID, imageExtention, captionText, authorName) {
+        this.localPath = `/public/images/${imageID}.${imageExtention}`;
+        this.captionText = captionText
+        this.authorName = authorName
+        this.id = imageID;
+    }
+
+}
 
 // ----------------------------------------------------------------------
 
@@ -7,12 +25,22 @@ function getPostByid(id) {
 
 }
 
-function submitPost({image: imageURL, caption: captionText}, authorName) {
-
+function submitPost(imageID, imageExtention, captionText, authorName) {
+    posts.push(new Post(imageID, imageExtention, captionText, authorName));
+    updatePosts();
 }
 
-function getPostsByCount(count) {
-
+function getPosts() {
+    return posts;
 }
 
 // ----------------------------------------------------------------------
+
+function updatePosts() {
+    fs.writeFileSync('./api/posts.json', JSON.stringify(posts, null, 4));
+}
+
+// ----------------------------------------------------------------------
+
+module.exports.submitPost = submitPost;
+module.exports.getPosts = getPosts;
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 const express = require('express');
 const fs = require('fs');
+
+const imgbrd = require('./api/api');
 
 // ----------------------------------------------------------------------
 
@@ -9,6 +13,8 @@ app.listen(6969, () => { console.log("Started!"); })
 
 // ----------------------------------------------------------------------
 
+app.use(bodyParser.raw());
+
 app.set("view-engine", "ejs");
 
 app.use(express.static('pages'));
@@ -16,6 +22,10 @@ app.use(express.static('pages/users'));
 
 app.use(express.static('public/css'))
 app.use(express.static('public/audio'))
+
+// ----------------------------------------------------------------------
+
+var fileId = newCode();
 
 // ----------------------------------------------------------------------
 
@@ -44,8 +54,59 @@ app.get('*', (req, res) => {
             res.render(page('board'));
         break;
 
+        case "/api/posts":
+            res.send(imgbrd.getPosts());
+        break;    
+
         default:
             res.render(page('404'));
+        break;
+
+    }
+
+});
+
+app.post('*', (req, res) => {
+
+    if(!req.path.startsWith('/api/')) return res.sendFile(__dirname + '/public/404');
+    var path = req.path.toLowerCase().slice(4);
+
+    console.log(`\nNew API Request: ${path}`);
+
+    switch(path) {
+
+        case "/upload":
+
+        var form = new multiparty.Form();
+
+            form.parse(req, function(err, fields, files) {
+
+                if(!fields || !files) return res.redirect('/board?e=Missing Fields Or Files')
+
+                if(!fields.caption) return res.redirect('/board?e=No Caption Provided');
+                if(!fields.caption[0]) return res.redirect('/board?e=No Caption Provided');
+                if(fields.caption[0].length < 3 || fields.caption[0].length > 250) return res.redirect('/board?e=Caption Too Long Or Too Short');
+
+                if(!files.image) return res.redirect('/board?e=No Image Provided');
+                if(!files.image[0]) return res.redirect('/board?e=No Image Provided');
+
+                var _fileId = fileId;
+
+                fs.copyFileSync(files.image[0].path, `./public/images/${_fileId}.${files.image[0].headers['content-type'].split('/')[1]}`);
+                imgbrd.submitPost(_fileId, files.image[0].headers['content-type'].split('/')[1], fields.caption[0], 'Anon');
+
+                return res.redirect('/board');
+
+            });
+
+        break;
+
+        case "/posts":
+            res.send(imgbrd.getPosts());
+        break;
+
+        default:
+            res.sendFile(__dirname + '/public/404');
         break;
 
     }
@@ -60,4 +121,34 @@ function page(pageName) {
 
 function file(fileName) {
     return `${__dirname}/public/${fileName}`;
+}
+
+function newCode() {
+    
+    var posts = imgbrd.getPosts();
+
+    var alphabet = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM-_";
+
+    var codeUsed = true;
+
+    while(codeUsed) {
+
+        var code = "";
+
+        for(var i = 0; i < 15; i++) {
+            code += alphabet[Math.floor(Math.random() * Math.floor(alphabet.length))];
+        }
+
+        // Is Code Used?
+
+        for(var i in posts) {
+            if(posts[i].id == code) codeUsed = true;
+        }
+
+        codeUsed = false;
+
+    }
+
+    return code;
+
 }
